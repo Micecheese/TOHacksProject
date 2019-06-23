de116eb819e0c337e0fb55bbd39759c8d3cca192
Avery <averyshum62@gmail.com>
2019-06-22 20:16:30 -0400

    Added write player names to JSON file

--- update_player_list.py
+++ update_player_list.py
@@ -7,6 +7,11 @@ inactive_player_info = get_inactive_players()
 active_players = [info['full_name'] for info in active_player_info]
 inactive_players = [info['full_name'] for info in inactive_player_info]
 
+active_players.sort()
+inactive_players.sort()
+
 all_players = {'active_players' : active_players, 'inactive_players' : inactive_players}
 
+with open('player_names.json', 'w+') as f:
+    f.write(dumps(all_players))
 
