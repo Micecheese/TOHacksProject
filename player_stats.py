9777cb524d0fa5741c4802fea323048b3d3b7930
Michael Zheng <cm2zheng@edu.uwaterloo.ca>
2019-06-23 00:04:17 -0400

    updated reply_post, put season player info in a csv file for reliable use

--- player_stats.py
+++ player_stats.py
@@ -1,25 +1,27 @@
 from nba_api.stats.endpoints import playercareerstats, commonallplayers, commonplayerinfo
 import pandas
 
-
-def points_per_game(id):
+def make_csv(id):
     player_info = playercareerstats.PlayerCareerStats(player_id=id)
     df = player_info.get_data_frames()[0]
-    #df.to_csv('Stats.csv')
-
+    df.to_csv('Stats.csv')
     read_df = pandas.read_csv('Stats.csv')
-    print(read_df)
+    raw_values = read_df.sum(axis=0, skipna=True)
+    print(raw_values)
+    return raw_values
 
-    raw_values = read_df.sum(axis = 0, skipna=True)
 
-    print(raw_values.iloc[7])
-
+def points_per_game(id):
+    raw_values = make_csv(id)
     total_points = raw_values.iloc[27]
     total_games = raw_values.iloc[7]
     return round(total_points/total_games, 1)
 
+def assists_per_game(id):
+    raw_values = make_csv(id)
 
 
+print(points_per_game(202695))
 
 
 
